bbbb5fc4e33b71572703ad275d12c69655e4af4f
Patrick <patrick.williams.1396@gmail.com>
2021-08-02 23:30:39 +0100

    Issue in the printing and in generation of the move mask

--- Game_Of_Ur.f90
+++ Game_Of_Ur.f90
@@ -19,21 +19,137 @@ program game_of_ur
   type(piece), dimension(0:13)       :: counters
   integer :: i
   integer, dimension(0:1, 1:14, 0:2) :: pos_map
-  integer                            :: turn = 0
+  integer, dimension(0:6)            :: move_mask
   integer                            :: roll
-
+  integer                            :: round, turn
+ 
+  !setup
   call Init_Board(board, off_board)
 
   call Init_Counters(counters)
 
   call Init_Pos_Map(pos_map)
-
   
   call Random_Seed()
-  roll = Dice_Roll()+Dice_Roll()+Dice_Roll()+Dice_Roll() 
-  call Print_Board(board, off_board, counters, roll)
+  
+
+  round = -1
+  call system("clear")
+
+  if (round.lt.0) then
+     round = Who_Goes_First()
+  end if
+  turn = modulo(round,2)
+
+  call Counter_Positions(board, off_board, counters, pos_map)
+
+  roll = Dice_Roll()+Dice_Roll()+Dice_Roll()+Dice_Roll()
+  move_mask(:) = 0
+  move_mask = Find_Valid_Moves(counters, board, pos_map, turn, roll)
+  call Print_Board(board, off_board, counters, move_mask, roll, turn)
 
 contains
+
+  function Find_Valid_Moves(counters, board, pos_map, turn, roll) result(move_mask)
+    implicit none
+    type(piece), dimension(0:13), intent(in)         :: counters
+    type(square), dimension(0:7,0:2), intent(inout)  :: board
+    integer, dimension(0:1, 1:14, 0:1), intent(in)   :: pos_map
+    integer, intent(in)                              :: roll
+    integer, intent(in)                              :: turn
+
+    integer, dimension(0:6)                          :: move_mask
+    integer                                          :: i
+    integer                                          :: check_pos, target_counter
+    integer                                          :: ind_1, ind_2
+
+
+    do i = 0+7*turn, 6+7*turn
+       check_pos = counters(i)%position + roll
+       if (check_pos.gt.15) then
+          move_mask(i) = 0
+          cycle
+       end if
+       
+       ind_1 = pos_map(0, check_pos, turn)
+       ind_2 = pos_map(1, check_pos, turn)
+
+       target_counter = board(ind_1, ind_2)%counter 
+       
+       if ((ind_1.eq.3).and.(ind_2.eq.1).and.(target_counter.ne.-1)) then
+          move_mask(i) = 0
+
+       elseif (target_counter .eq. -1) then
+          move_mask(i) = 1
+       elseif (modulo(target_counter, 7).eq.turn) then
+          move_mask(i) = 0
+       else
+          move_mask(i) = 1
+       end if
+       
+    end do
+
+    
+    
+  end function Find_Valid_Moves
+  
+  function Who_Goes_First() result(player)
+    implicit none
+
+    integer :: player
+    
+    integer :: w_roll, b_roll
+    logical :: found
+
+    found = .false.
+
+
+    do while (found.eqv..false.)
+       w_roll = Dice_Roll()+Dice_Roll()+Dice_Roll()+Dice_Roll()
+       print *, "White rolled: ", w_roll
+       print *, ""
+       b_roll = Dice_Roll()+Dice_Roll()+Dice_Roll()+Dice_Roll()
+       print *, "Black rolled: ", b_roll
+       print *, ""
+
+       if (w_roll.gt.b_roll) then
+          print *, "White goes first"
+          player = 0
+          found = .true.
+       elseif (b_roll.gt.w_roll) then
+          print *, "Black goes first"
+          player = 1
+          found = .true.
+       else
+          print *, "Draw, reroll"
+          found = .false.
+       end if
+       
+    end do
+
+  end function Who_Goes_First
+
+  subroutine Counter_Positions(board, off_board, counters, pos_map)
+    implicit none
+    type(square), dimension(0:7,0:2), intent(inout)  :: board
+    type(square), dimension(0:15,0:1), intent(inout) :: off_board
+    type(piece), dimension(0:13), intent(in)         :: counters
+    integer, dimension(0:1, 1:14, 0:1), intent(in)   :: pos_map
+    
+    integer                                       :: i
+    integer                                       :: ind_1, ind_2
+    
+    do i = 0,13
+       if (counters(i)%position.eq.0) then
+          off_board(modulo(i,7)+1,floor(i/7.0_dp))%counter = i
+       elseif (counters(i)%position.eq.15) then
+          off_board(modulo(i,7)+9,floor(i/7.0_dp))%counter = i
+!       else
+          !         ind_1 = pos_map(floor(i/7.0_dp)
+       end if
+    end do
+    
+  end subroutine Counter_Positions
 
   subroutine Init_Board(board, off_board)
     implicit none
@@ -58,7 +174,7 @@ contains
           off_board(j, i)%text = "__"
        end do
        off_board(0,i)%text = "S:"
-       off_board(7,i)%text = "F:"
+       off_board(8,i)%text = "F:"
     end do
     
   end subroutine Init_Board
@@ -92,114 +208,126 @@ contains
 
     !Mapping of white positions
     pos_map(0,1,0) = 3
-    pos_map(0,1,1) = 2
+    pos_map(1,1,0) = 2
 
     pos_map(0,2,0) = 2
-    pos_map(0,2,1) = 2
+    pos_map(1,2,0) = 2
 
     pos_map(0,3,0) = 1
-    pos_map(0,3,1) = 2
+    pos_map(1,3,0) = 2
 
     pos_map(0,4,0) = 0
-    pos_map(0,4,1) = 2
+    pos_map(1,4,0) = 2
 
     pos_map(0,5,0) = 0
-    pos_map(0,5,1) = 1
+    pos_map(1,5,0) = 1
 
     pos_map(0,6,0) = 1
-    pos_map(0,6,1) = 1
+    pos_map(1,6,0) = 1
 
     pos_map(0,7,0) = 2
-    pos_map(0,7,1) = 1
+    pos_map(1,7,0) = 1
 
     pos_map(0,8,0) = 3
-    pos_map(0,8,1) = 1
+    pos_map(1,8,0) = 1
 
     pos_map(0,9,0) = 4
-    pos_map(0,9,1) = 1
+    pos_map(1,9,0) = 1
 
     pos_map(0,10,0) = 5
-    pos_map(0,10,1) = 1
+    pos_map(1,10,0) = 1
 
     pos_map(0,11,0) = 6
-    pos_map(0,11,1) = 1
+    pos_map(1,11,0) = 1
 
     pos_map(0,12,0) = 7
-    pos_map(0,12,1) = 1
+    pos_map(1,12,0) = 1
 
     pos_map(0,13,0) = 7
-    pos_map(0,13,1) = 2
+    pos_map(1,13,0) = 2
 
     pos_map(0,14,0) = 6
-    pos_map(0,14,1) = 2
+    pos_map(1,14,0) = 2
 
 
     !Mapping of black positions
-    pos_map(0,1,0) = 3
-    pos_map(0,1,1) = 0
+    pos_map(0,1,1) = 3
+    pos_map(1,1,1) = 0
 
-    pos_map(0,2,0) = 2
-    pos_map(0,2,1) = 0
+    pos_map(0,2,1) = 2
+    pos_map(1,2,1) = 0
 
-    pos_map(0,3,0) = 1
-    pos_map(0,3,1) = 0
+    pos_map(0,3,1) = 1
+    pos_map(1,3,1) = 0
 
-    pos_map(0,4,0) = 0
     pos_map(0,4,1) = 0
+    pos_map(1,4,1) = 0
 
-    pos_map(0,5,0) = 0
-    pos_map(0,5,1) = 1
+    pos_map(0,5,1) = 0
+    pos_map(1,5,1) = 1
 
-    pos_map(0,6,0) = 1
     pos_map(0,6,1) = 1
+    pos_map(1,6,1) = 1
 
-    pos_map(0,7,0) = 2
-    pos_map(0,7,1) = 1
+    pos_map(0,7,1) = 2
+    pos_map(1,7,1) = 1
 
-    pos_map(0,8,0) = 3
-    pos_map(0,8,1) = 1
+    pos_map(0,8,1) = 3
+    pos_map(1,8,1) = 1
 
-    pos_map(0,9,0) = 4
-    pos_map(0,9,1) = 1
+    pos_map(0,9,1) = 4
+    pos_map(1,9,1) = 1
 
-    pos_map(0,10,0) = 5
-    pos_map(0,10,1) = 1
+    pos_map(0,10,1) = 5
+    pos_map(1,10,1) = 1
 
-    pos_map(0,11,0) = 6
-    pos_map(0,11,1) = 1
+    pos_map(0,11,1) = 6
+    pos_map(1,11,1) = 1
 
-    pos_map(0,12,0) = 7
-    pos_map(0,12,1) = 1
+    pos_map(0,12,1) = 7
+    pos_map(1,12,1) = 1
 
-    pos_map(0,13,0) = 7
-    pos_map(0,13,1) = 0
+    pos_map(0,13,1) = 7
+    pos_map(1,13,1) = 0
 
-    pos_map(0,14,0) = 6
-    pos_map(0,14,1) = 0
+    pos_map(0,14,1) = 6
+    pos_map(1,14,1) = 0
 
   end subroutine Init_Pos_Map
   
-  subroutine Print_Board(board, off_board, counters, roll)
+  subroutine Print_Board(board, off_board, counters, move_mask, roll, turn)
     implicit none
     type(square), dimension(0:7,0:2), intent(in)  :: board
     type(square), dimension(0:15,0:1), intent(in) :: off_board
     type(piece), dimension(0:13), intent(in)      :: counters
+    integer, dimension(0:6), intent(in)           :: move_mask
     integer, intent(in)                           :: roll
+    integer, intent(in)                           :: turn
     integer                                       :: i, j, index
-    
-    call system("clear")
 
-    write(*,*) roll
+    
+    
+    write(*,*) ""
+    if (turn.eq.0) then
+       write(*,*) "White rolled: ", roll
+    else
+       write(*,*) "Black rolled: ", roll
+    end if
     do i = 0, 15
-       index = off_board(i,0)%counter
+       index = off_board(i,1)%counter
        if (index.eq.-1) then
           write(*, '(A2)', advance = 'No') off_board(i, 0)%text
+       elseif (turn.eq.1) then
+          write(*, '(A2)', advance = 'No') counters(index)%text(0+move_mask(index-7))
        else
           write(*, '(A2)', advance = 'No') counters(index)%text(0)
        end if
+       print *, index
        write(*, '(A2)', advance = 'No') " "
     end do
+   
     write(*,*)
+    print *, move_mask(:)
     write(*,*)
    
     do i = 0,2
@@ -214,11 +342,15 @@ contains
 
     do i = 0, 15
        index = off_board(i,0)%counter
+
        if (index.eq.-1) then
           write(*, '(A2)', advance = 'No') off_board(i, 0)%text
+       !elseif (turn.eq.0) then
+       !   write(*, '(A2)', advance = 'No') counters(index)%text(0+move_mask(index))
        else
           write(*, '(A2)', advance = 'No') counters(index)%text(0)
        end if
+       print *, index
        write(*, '(A2)', advance = 'No') " "
     end do
     write(*,*)
